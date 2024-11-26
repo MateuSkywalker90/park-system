84a62b2dc2969a977460dac3163a7fd0784bb04a
Mateus Oliveira <mateuskywalker90@gmail.com>
2024-11-26 13:44:04 -0400

    adding auditid config in user entity

--- src/main/java/com/moliveira/demo_park_api/entity/User.java
+++ src/main/java/com/moliveira/demo_park_api/entity/User.java
@@ -4,6 +4,11 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -12,6 +17,7 @@ import java.util.Objects;
 @Getter @Setter @NoArgsConstructor
 @Entity
 @Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
 public class User implements Serializable {
 
     @Id
@@ -29,15 +35,19 @@ public class User implements Serializable {
     @Column(name = "role", nullable = false, length = 25)
     private Role role = Role.ROLE_CLIENT;
 
+    @CreatedDate
     @Column(name = "create_date")
     private LocalDateTime createDate;
 
+    @LastModifiedDate
     @Column(name = "update_date")
     private LocalDateTime updateDate;
 
+    @CreatedBy
     @Column(name = "created_by")
     private String createdBy;
 
+    @LastModifiedBy
     @Column(name = "updated_by")
     private String updatedBy;
 
