1c3e4f837c0c925d31c9f29b0abb881349b0397e
Mateus Oliveira <mateuskywalker90@gmail.com>
2024-11-26 15:33:09 -0400

    adding authorization by user role

--- src/main/java/com/moliveira/demo_park_api/web/controller/UserController.java
+++ src/main/java/com/moliveira/demo_park_api/web/controller/UserController.java
@@ -16,6 +16,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
 
@@ -54,6 +55,7 @@ public class UserController {
                     )
             })
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') OR (hasRole('CLIENT') AND #id == authentication.principal.id)")
     public ResponseEntity<UserResponseDto> getById(@PathVariable Long id) {
         User responseUser = userService.searchById(id);
         return ResponseEntity.ok(UserMapper.toDto(responseUser));
