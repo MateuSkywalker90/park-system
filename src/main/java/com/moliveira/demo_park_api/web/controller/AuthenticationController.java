d66021e2bf8a314dc915f66e6e767e11076531b0
Mateus Oliveira <mateuskywalker90@gmail.com>
2024-12-02 08:43:37 -0400

    created the authentication documentation

--- src/main/java/com/moliveira/demo_park_api/web/controller/AuthenticationController.java
+++ src/main/java/com/moliveira/demo_park_api/web/controller/AuthenticationController.java
@@ -3,7 +3,13 @@ package com.moliveira.demo_park_api.web.controller;
 import com.moliveira.demo_park_api.jwt.JwtToken;
 import com.moliveira.demo_park_api.jwt.JwtUserDetailsService;
 import com.moliveira.demo_park_api.web.dto.UserLoginDto;
+import com.moliveira.demo_park_api.web.dto.UserResponseDto;
 import com.moliveira.demo_park_api.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +19,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Authentication", description = "API authentication resource")
 @Slf4j
 @RequiredArgsConstructor
 @RestController
@@ -27,6 +35,16 @@ public class AuthenticationController {
     private final JwtUserDetailsService detailsService;
     private final AuthenticationManager authenticationManager;
 
+    @Operation(summary = "Authentication in API", description = "API authentication resource",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Authentication completed successfully, returning a bearer token",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDto.class))),
+                    @ApiResponse(responseCode = "400", description = "Invalid credentials",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "422", description = "Invalid fields",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))
+                    )
+            })
     @PostMapping("/auth")
     public ResponseEntity<?> authenticate(@RequestBody @Valid UserLoginDto dto, HttpServletRequest request) {
         log.info("Authentication process by login {}", dto.getUsername());
---
+++ src/test/java/com/moliveira/demo_park_api/AuthenticationIT.java
@@ -0,0 +1,4 @@
+package com.moliveira.demo_park_api;
+
+public class AuthenticationIT {
+}
